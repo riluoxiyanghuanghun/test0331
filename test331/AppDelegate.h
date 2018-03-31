35e46bb209b07d999cc14b11854474c95d90faa2
HLZGW <hlzg@HLZGW.local>
2018-03-31 15:57:28 +0800

    xxx

---
+++ test331/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test331
+//
+//  Created by HLZGW on 2018/3/31.
+//  Copyright © 2018年 HLZGW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test331/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test331
+//
+//  Created by HLZGW on 2018/3/31.
+//  Copyright © 2018年 HLZGW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
